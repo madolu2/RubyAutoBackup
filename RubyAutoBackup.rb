81cf01ca16a12eaf8774d3febefbce0971b19710
madolu2 <49994326+madolu2@users.noreply.github.com>
2019-12-09 07:05:24 +0700

    Refactor BackupFunc()/Add HashFill()/Refactor all output

--- RubyAutoBackup.rb
+++ RubyAutoBackup.rb
@@ -3,6 +3,14 @@
 #directory and places it in the specified location.
 #2. Writes his actions  to a log file.
 
+def HashFill(source_path)
+  Dir.foreach(source_path) do |file|
+    if file != '..' and file != '.'
+      $mtime[file] = File.mtime(source_path + file)
+    end
+  end
+end
+
 def CopyFile(source_path, target_path)
   File.open(target_path, 'w')
 
@@ -16,42 +24,43 @@ end
 def BackupFunc(source_path, target_path, user_action = false)
   Dir.foreach(source_path) do |file|
     if file != '..' and file != '.'
+      if File.mtime(source_path + file) > $mtime[file]
+        $mtime[file] = File.mtime(source_path + file)
 
-      target_path += '/' + file
-      source_path += '/' + file
+        CopyFile(source_path + file, target_path + file)
 
-      CopyFile(source_path, target_path)
-
-      target_path = '/home/madolu2/RubySVN/target'
-      source_path = '/home/madolu2/RubySVN/source'
-
-      File.open($log_file, 'a') do |logfile|
-        if user_action == false
-          logfile.puts "#{Time.now} Auto backup"
-          puts 'auto backup'
-        else
-          logfile.puts "#{Time.now} User backup"
-          puts 'GOT IT'
+        File.open($log_file, 'a') do |logfile|
+          if user_action == false
+            logfile.puts "#{Time.now} Auto backup"
+            puts "auto backup"
+          else
+            logfile.puts "#{Time.now} User backup"
+            puts "DONE\n"
+          end
         end
       end
     end
   end
 end
 
-$log_file = '/home/madolu2/RubySVN/logfile.txt'#Logfile.txt path
-threads = []
-sleep_time = 5*60
+$mtime = Hash.new #Hash file last change date
+$log_file = "/home/madolu2/RubyAutoBackup/logfile.txt"#Logfile.txt path
+threads = [] #Threads array
+sleep_time = 5*60 #Sleep timer for BackupFunc()
+source_path = "/home/madolu2/RubyAutoBackup/source/"#Ur source directory
+target_path = "/home/madolu2/RubyAutoBackup/target/"#Ur target directory
 
-source_path = '/home/madolu2/RubySVN/source'#Ur source directory
-target_path = '/home/madolu2/RubySVN/target'#Ur target directory
+HashFill(source_path)#Fill $mtime hash
 
 threads << Thread.new {
-  puts "AUTO BACKUP AFTER EVERY 10 MIN\nPRESS '>' TO BACKUP"
+  puts "AUTO BACKUP AFTER EVERY 10 MIN\n"
   while true
+    puts "PRESS '>' TO BACKUP"
+
     user_choice = gets.chomp
 
     if user_choice == '>'
-     BackupFunc(source_path, target_path, true)
+      BackupFunc(source_path, target_path, true)
     end
   end
 }
